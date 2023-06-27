7ef8ce3e3faa2d6b2f578edc0788376e09a3ea61
sinedied <noda@free.fr>
2023-06-27 16:35:09 +0200

    chore: add words count

--- programmez/create-docx.sh
+++ programmez/create-docx.sh
@@ -3,3 +3,6 @@ set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 for f in *.md; do cat "$f"; echo; done | pandoc -f markdown -t docx -o dossier-vs-code.docx
+
+words=$(wc -w ./*.md | grep total | awk '{print $1}')
+echo "Nombre de mots dans le dossier: $words"
