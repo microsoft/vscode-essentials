c0e898bd4f360c680b70216a88a8fc6d6926caba
Christopher MANEU <git@maneu.org>
2023-09-15 16:48:31 +0200

    tooling: add number of signs

--- programmez/create-docx.sh
+++ programmez/create-docx.sh
@@ -5,4 +5,6 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 for f in *.md; do cat "$f"; echo; done | pandoc -f markdown -t docx -o dossier-vs-code.docx
 
 words=$(wc -w ./*.md | grep total | awk '{print $1}')
-echo "Nombre de mots dans le dossier: $words"
+signs=$(wc -c ./*.md | grep total | awk '{print $1}')
+
+echo "Nombre de mots dans le dossier: $words pour $signs signes."
