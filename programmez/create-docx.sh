441707612fe914e826ce01dff63f22b97b810080
sinedied <noda@free.fr>
2023-10-05 14:51:08 +0200

    chore: change highlighting style

--- programmez/create-docx.sh
+++ programmez/create-docx.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
-for f in *.md; do cat "$f"; echo; done | pandoc -f markdown -t docx -o dossier-vs-code.docx
+for f in *.md; do cat "$f"; echo; done | pandoc --highlight-style haddock -f markdown -t docx -o dossier-vs-code.docx
 
 words=$(wc -w ./*.md | grep total | awk '{print $1}')
 echo "Nombre de mots dans le dossier: $words"
